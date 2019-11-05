e4ee5caa4de6dbb8f9767a446d4516252594431c
bradybod <bradybod@gmail.com>
2019-11-05 16:33:33 -0700

    pushing to go home

---
+++ Hw6/Renderer.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Brady Bodily on 11/5/19.
+//
+
+#ifndef HW6_RENDER_HPP
+#define HW6_RENDER_HPP
+
+#endif //HW6_RENDER_HPP
---
+++ Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/LifeSimulator.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/LifeSimulator.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/PatternAcorn.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/PatternAcorn.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/PatternBlinker.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/PatternBlinker.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/PatternBlock.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/PatternBlock.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/PatternGlider.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/PatternGlider.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/PatternGosperGliderGun.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/PatternGosperGliderGun.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/RendererConsole.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/RendererConsole.cpp.o"
+  "/Users/brady/CLionProjects/CS3460-CPP/Hw6/main.cpp" "/Users/brady/CLionProjects/CS3460-CPP/Hw6/cmake-build-debug/CMakeFiles/ConwaysLife.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
