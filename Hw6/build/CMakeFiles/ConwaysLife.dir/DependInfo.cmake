15ae10f453e44c0810bfeacdf37675259d2f06ad
bbod <bradybod@gmail.com>
2019-11-05 20:19:00 -0700

    Patterns are completed, need to work on life simulator.

---
+++ Hw6/build/CMakeFiles/ConwaysLife.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bradybodily/Repositories/CS3460/Hw6/LifeSimulator.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/LifeSimulator.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/PatternAcorn.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/PatternAcorn.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/PatternBlinker.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/PatternBlinker.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/PatternBlock.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/PatternBlock.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/PatternGlider.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/PatternGlider.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/PatternGosperGliderGun.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/PatternGosperGliderGun.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/RendererConsole.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/RendererConsole.cpp.o"
+  "/Users/bradybodily/Repositories/CS3460/Hw6/main.cpp" "/Users/bradybodily/Repositories/CS3460/Hw6/build/CMakeFiles/ConwaysLife.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Hw6/build/CMakeFiles/ConwaysLife.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConwaysLife.dir/LifeSimulator.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/PatternAcorn.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/PatternBlinker.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/PatternBlock.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/PatternGlider.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/PatternGosperGliderGun.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/RendererConsole.cpp.o"
+  "CMakeFiles/ConwaysLife.dir/main.cpp.o"
+  "ConwaysLife"
+  "ConwaysLife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConwaysLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
